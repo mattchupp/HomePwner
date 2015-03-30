756b1348c622e18421b7e73b4fe423d6adc9b934
Matthew Chupp <mattlchupp@gmail.com>
2015-03-30 12:34:04 -0500

    .xib connections to MCItemCell.h

---
+++ MCItemCell.h
@@ -0,0 +1,19 @@
+//
+//  MCItemCell.h
+//  HomePwner
+//
+//  Created by Matthew Chupp on 3/30/15.
+//  Copyright (c) 2015 MattChupp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface MCItemCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *thumbnailView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *serialNumberLabel;
+@property (weak, nonatomic) IBOutlet UILabel *valueLabel;
+
+@end
